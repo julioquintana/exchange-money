56f62929e8f8e4abf6cb55427238f071527cfae5
Julio Quintana <julio.quintanasilva@cencosud.cl>
2022-06-08 20:47:49 -0400

    commit initial functional

---
+++ src/main/java/cl/exchangemoney/pruebatest/PruebaTestApplication.java
@@ -0,0 +1,13 @@
+package cl.exchangemoney.pruebatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/exchangemoney/pruebatest/PruebaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.exchangemoney.pruebatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
